8661ae02ad645e440a4b55cb0ed59a25150b58ae
pinomodz <nihstock40@gmail.com>
2021-10-08 04:28:51 +0700

    Full Bug Work All

---
+++ message/donasi.js
@@ -0,0 +1,17 @@
+exports.dodo = (prefix) => {
+return`╔══✪〘 INFO 〙✪══
+║
+╠═══════════════════════════
+╠➥ *DAFTAR SEWA BOT :*
+╠➥ *SEWA : 10K/GRUP (MANEN)*
+╠➥ *BUAT : 35K (BISA JADI OWNER)*
+╠➥ *PEMBAYARAN BISA MELALUI :*
+╠➥ *GOPAY/PULSA*
+╠═══════════════════════════
+╠➥ *JIKA MINAT IKLAN DIATAS*
+╠➥ *HARAP HUBUNGI NOMOR DIBAWAH :*
+╠➥ *https://wa.me/message/4QGPG3UPJEUUC1*
+║
+╚═〘  ZAM BOT 〙✪══
+`
+}
